55cef119c8ae9bb67a8d62024c9a6b4826fdd6ee
Clement Cardon <clement.cardon3.etu@univ-lille.fr>
2025-07-25 13:55:58 +0200

    Super important !

--- src/main/java/net/carm/chcarrefour/Chcarrefour.java
+++ src/main/java/net/carm/chcarrefour/Chcarrefour.java
@@ -19,6 +19,6 @@ public class Chcarrefour implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello les ch'timis !");
 	}
 }
